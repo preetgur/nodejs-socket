0fd09c6f34e089b6b760b1068f45d49539adc88c
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-01-17 14:44:57 +0530

    socket end end

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-var CryptoJS = require("crypto-js");
 
 app.use(cors());
 
@@ -18,14 +17,102 @@ const io = new Server(server, {
 
 let clients = {};
 let messageQueue = [];
+let connectedUsers = {};
 
 io.on("connection", (socket) => {
+
+
+  const { username } = socket.handshake.query;
+  clients[socket.id] = username;
+  io.emit('updateUsers', clients);
+
+  
+  socket.on('join', roomName => {
+    socket.join(roomName);
+    console.log(`########## rooom joined by ${socket.id} with room ${roomName} `)
+    io.to(socket.id).emit('joinRoomSuccess', roomName);
+    
+  });
+
+  
   socket.on("login", (username) => {
     console.log("#### user login ####", username, socket.id);
     clients[username] = socket;
+    socket.emit("connected", {
+      msg: `user connected with socket.id : ${socket.id}`,
+      id: socket.id,
+    });
+
     deliverPendingMessages(username);
     console.log("message queue ###", messageQueue);
   });
+
+ // handle user disconnection
+ socket.on('disconnect', () => {
+  console.log("#### user disconnected ####",socket.id)
+  delete clients[socket.id];
+  io.emit('updateUsers', clients);
+});
+
+  // // handle connection request
+  // socket.on('connectToUser', userId => {
+    
+  //   console.log("### clients ####",clients)
+  //   // for (const iterator of clients) {
+  //     console.log("##### iterator ####",clients[userId])
+  //   // }
+  //   // const targetUser = clients.find(user => user.id === userId);
+  //   // console.log("##### target id ######",targetUser)
+  //   if (clients[userId]) {
+  //   console.log("##### socket id ######",socket.id)
+
+  //     io.to(userId).emit('connectionRequest', {
+  //       from: username,
+  //       id: socket.id
+  //     });
+  //   }
+  // });
+
+
+    // handle connection request
+    socket.on('connectToUser', userId => {
+      const targetUser = clients[userId];
+    console.log("##### target id ######",targetUser.id)
+
+      if (targetUser) {
+        io.to(userId).emit('connectionRequest', {
+          from: username,
+          id: socket.id
+        });
+      }
+    });
+
+    // request rooms 
+    socket.on('requestRooms', () => {
+      console.log("##### list roooms ######",socket.rooms)
+      socket.emit('ListRooms', socket.rooms);
+    });
+
+   // handle connection acceptance
+   socket.on('acceptConnection', userId => {
+    const targetUser = clients[userId];
+    let roomName;
+    console.log("#### accept connectiojn #####",targetUser)
+    if (targetUser) {
+      roomName = `${socket.id}-${userId}`;
+      socket.join(roomName);
+      // io.to(userId).emit('connectToRoom', roomName);
+    
+
+      io.to(userId).emit('joinRoom', roomName);
+      io.to(roomName).emit('connectionEstablished', {
+        room: roomName,
+        users: [clients[socket.id], targetUser]
+      });
+    }
+
+  });
+
 
   socket.on("send message", (recipient, message) => {
     console.log("####### send message #####", recipient, message);
@@ -33,13 +120,13 @@ io.on("connection", (socket) => {
     // console.log("####### recipient socket ######",recipientSocket)
     if (recipientSocket) {
       console.log("### inside if ###", recipientSocket.emit());
-      let bytes = CryptoJS.AES.decrypt(message, "secret key 123");
-      let originalText = bytes.toString(CryptoJS.enc.Utf8);
 
-      console.log(originalText); // 'my message'
-      recipientSocket.emit("receive_message", originalText);
+      recipientSocket.emit("receive_message", message);
     } else {
       console.log(`User ${recipient} is not online. Adding message to queue.`);
+      socket.emit("notconnected", {
+        msg: `User ${recipient} is not online`,
+      });
       messageQueue.push({
         recipient: recipient,
         message: message,
@@ -48,7 +135,21 @@ io.on("connection", (socket) => {
       console.log("######## messge queure ######", messageQueue);
     }
   });
+
+    // handle messages
+    socket.on('sendMessage', (room, message) => {
+      io.to(room).emit('newMessage', {
+        from: clients[socket.id],
+        message
+      });
+
+    });
+
+
 });
+
+
+
 
 function deliverPendingMessages(username) {
   const pendingMessages = messageQueue.filter((m) => m.recipient === username);
